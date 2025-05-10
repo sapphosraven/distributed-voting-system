9e954b58363b7ff621648b82fe5a9aa8782eefd8
zik <113235622+sapphosraven@users.noreply.github.com>
2025-05-11 02:38:34 +0500

    fixed postgres host

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   process.env.POSTGRES_USER || 'user',
   process.env.POSTGRES_PASSWORD || 'pass',
   {
-    host: process.env.POSTGRES_HOST || 'postgres',
+    host: process.env.POSTGRES_HOST || 'voting_postgres',
     dialect: 'postgres',
     logging: false,
   }
