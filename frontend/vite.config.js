89785771ab0db94e6e0eb9aeb629b97a0fc23172
zik <113235622+sapphosraven@users.noreply.github.com>
2025-05-15 10:51:35 +0500

    yes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
+      "/api": {
+        target: "https://localhost:5002", // changed to https
         changeOrigin: true,
-        secure: false,
+        secure: false, // allow self-signed certs
         ws: true,
       },
     },
